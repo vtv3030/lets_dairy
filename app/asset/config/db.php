05f3543196d6c80922a307e3871bdd44c1b38e0e
Vaibhav Varpe <vtv3030@gmail.com>
2023-12-17 23:21:15 +0530

    update

--- app/asset/config/db.php
+++ app/asset/config/db.php
@@ -7,7 +7,7 @@ $servername = $env["db_host"];
 $username =  $env["db_user"];
 $password =  $env["db_pass"];
 
-if ($_SERVER['SERVER_NAME'] == "www.letsdiry.in"){
+if ($_SERVER['SERVER_NAME'] == "www.letsdairy.in"){
   $database =  $env["db_live_name"];
 } else {
   $database =  $env["db_test_name"];
@@ -20,9 +20,6 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-echo $database
-
 
 ?>
 
